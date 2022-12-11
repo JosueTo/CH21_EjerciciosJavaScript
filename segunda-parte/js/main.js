01b91a0f2d16d4e6efe0bcff073268774c0fd3ac
Josue Torres <josuetolvera@gmail.com>
2022-12-10 20:14:41 -0600

    Ejercicio número #2 de la segunda parte terminado

--- segunda-parte/js/main.js
+++ segunda-parte/js/main.js
@@ -4,6 +4,12 @@ let numeroMayor3 = document.getElementById("numeroMayor3");
 let alertMayor = document.getElementById("alertMayor");
 let btnMayor = document.getElementById("btnMayor");
 
+let numeroMenor1 = document.getElementById("numeroMenor1");
+let numeroMenor2= document.getElementById("numeroMenor2");
+let numeroMenor3 = document.getElementById("numeroMenor3");
+let alertMenor = document.getElementById("alertMenor");
+let btnMenor = document.getElementById("btnMenor");
+
 // Solicitar 3 números (entre el 1 y el 100) y definir cual es el mayor
 btnMayor.addEventListener("click", function(event) {
   let num1 = parseInt(numeroMayor1.value);
@@ -24,3 +30,20 @@ btnMayor.addEventListener("click", function(event) {
 })
 
 // Solicitar 3 números (entre el 1 y el 100) y definir el menor de tres números
+btnMenor.addEventListener("click", function(event) {
+  let num1 = parseInt(numeroMenor1.value);
+  let num2 = parseInt(numeroMenor2.value);
+  let num3 = parseInt(numeroMenor3.value);
+  let smallerNumber;
+  let numbers = [];
+  numbers.push(num1, num2, num3);
+
+  numbers.forEach(number => {
+    if (smallerNumber <= number) return;
+      else {
+      smallerNumber = number;
+    }
+  })
+
+  alertMenor.innerHTML = smallerNumber;
+})
